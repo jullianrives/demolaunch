868d04cc4732ce0bc074c04a56e671ec7d95aa6f
Jullian <42753251+jullianrives@users.noreply.github.com>
2018-12-03 17:14:49 -0500

    Demo a6 project launch

---
+++ assets/js/a6.js
@@ -0,0 +1,20 @@
+console.log("test");
+ $(document).ready(function() {
+//do not do anything till page is loaded
+
+$("#theTarget").skippr();
+
+$("#theTarget1").skippr();
+
+$("#theTarget_lm1").skippr();
+
+$("#theTarget_lm2").skippr();
+
+$("#theTarget_lm3").skippr();
+
+$("#theTarget_lm4").skippr();
+
+$("#theTarget_lm5").skippr();
+
+});
+//closes entire function
